6e889095023107937099be0b12a475936a3535c1
gxldCptRick <andreshcar@live.com>
2016-07-20 22:17:48 -0600

    adding some functionality to the miniGame page and changing styles to center the canvas and to add a new font style to the domain

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,20 @@
 var canvas = document.getElementById("photo"),
     context = canvas.getContext('2d');
 
-    context.beginPath();
-    context.arc(
-        75,
-        75,
-        75,
-        0,
-        Math.PI * 2,
-        true);
-    context.clip();
-    var img = new Image();
-    img.onload = function() {
-            context.drawImage(img, -60, 0);
-            context.lineWidth = 15;
-            context.strokeStyle = '#000';
-            context.stroke();
-        } ;    
-    img.src = '../images/gxldcptrickE.jpg';
+context.beginPath();
+context.arc(
+    75,
+    75,
+    75,
+    0,
+    Math.PI * 2,
+    true);
+context.clip();
+var img = new Image();
+img.onload = function() {
+        context.drawImage(img, -60, 0);
+        context.lineWidth = 15;
+        context.strokeStyle = '#000';
+        context.stroke();
+    } ;    
+img.src = '../images/gxldcptrickE.jpg';
--- scripts/miniGame.js
+++ scripts/miniGame.js
@@ -1,9 +1,75 @@
-function miniGame() {
-    this.start = function() {
-       
-   };
-    this.interval = setInterval(this.start(), 1000); 
+var canvas = document.getElementById("stuff"),
+    context = canvas.getContext('2d'),
+    square = {
+        xPos: 10,
+        yPos: 10,
+        size: 50
+    },
+    interval,
+    speed = 1,
+    up = false,
+    down = false,
+    left = false,
+    right = false;
+function draw () {
+    context.beginPath();
+    if((square.xPos + square.size) !== canvas.width && (square.yPos + square.size) !== canvas.height && square.xPos !== 0 && square.yPos !== 0){
+        if(up){
+            square.yPos -= speed;
+        }
+        else if(down){
+            square.yPos += speed;
+        }
+        else if(left){
+            square.xPos -= speed;
+        }
+        else if(right){
+            square.xPos += speed;
+        }
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        context.rect(square.xPos, square.yPos, square.size, square.size);
+        context.fillStyle = "#00ff33";
+        context.fill();
+    }
+    else {
+        square.xPos = 10;
+        square.yPos = 10;
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        context.rect(square.xPos, square.yPos, square.size, square.size);
+        context.fillStyle = "#00ff33";
+        context.fill();
+    }
 }
-$(document).ready(function () {
-    
-});
+interval = setInterval(draw, 20);
+document.addEventListener('keydown',function(e){
+        switch(e.keyCode){
+            case 65:
+                left = true;
+                break;
+            case 87:
+                up = true;
+                break;
+            case 68:
+                right = true;
+                break;
+            case 83:
+                down = true;
+                break;
+        }
+    });
+document.addEventListener('keyup',function(e){
+        switch(e.keyCode){
+            case 65:
+                left = false;
+                break;
+            case 87:
+                up = false;
+                break;
+            case 68:
+                right = false;
+                break;
+            case 83:
+                down = false;
+                break;
+        }
+    });
