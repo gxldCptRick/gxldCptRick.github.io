08ea8757f16744f507a33a0ac64f647b0cb32e14
gxldCptRick <andreshcar@live.com>
2016-08-08 10:57:40 -0600

    adding a button to start game easier

--- scripts/miniGame.js
+++ scripts/miniGame.js
@@ -1,5 +1,6 @@
 var timeSpace = document.getElementById("timer"),
-    gameInterval,
+    gameStartButton = document.getElementById("gameStart"),
+gameInterval,
     timerInterval,
     time,
     seconds = 0,
@@ -47,44 +48,54 @@ function timer(){
     timeSpace.innerHTML = time;
 }
 function draw () {
-        SquareMan.checkSquareMan();
-        drawEnemy(enemys);
-        if(collision){
-            clearInterval(gameInterval);
-            clearInterval(timerInterval);
-        }
+    SquareMan.checkSquareMan();
+    drawEnemy(enemys);
+    if(collision){
+        clearInterval(gameInterval);
+        clearInterval(timerInterval);
+        gameStartButton.classList.remove("hidden");
+    }
 }
-gameInterval = setInterval(draw, 1);
-timerInterval = setInterval(timer, 1000);
+function gameStart(){
+    collision = false;
+    hours = minutes = seconds = 0;
+    SquareMan.square.xPos = 627;
+    SquareMan.square.yPos = 277;
+    oscar.yPos = 40;
+    oscar.xPos = 40;
+    gameInterval = setInterval(draw, 1);
+    timerInterval = setInterval(timer, 1000);
+    gameStartButton.classList.add("hidden");
+}
 document.addEventListener('keydown',function(e){
-        switch(e.keyCode){
-            case 65:
-                left = true;
-                break;
-            case 87:
-                up = true;
-                break;
-            case 68:
-                right = true;
-                break;
-            case 83:
-                down = true;
-                break;
-        }
-    });
+    switch(e.keyCode){
+        case 65:
+            left = true;
+            break;
+        case 87:
+            up = true;
+            break;
+        case 68:
+            right = true;
+            break;
+        case 83:
+            down = true;
+            break;
+    }
+});
 document.addEventListener('keyup',function(e){
-        switch(e.keyCode){
-            case 65:
-                left = false;
-                break;
-            case 87:
-                up = false;
-                break;
-            case 68:
-                right = false;
-                break;
-            case 83:
-                down = false;
-                break;
-        }
-    });
+    switch(e.keyCode){
+        case 65:
+            left = false;
+            break;
+        case 87:
+            up = false;
+            break;
+        case 68:
+            right = false;
+            break;
+        case 83:
+            down = false;
+            break;
+    }
+});
