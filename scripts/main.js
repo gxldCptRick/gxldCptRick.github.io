93fac7f58dade767bb1aa7fb6f87318970fb24c0
Andres Carrera <andreshcar@live.com>
2020-10-09 00:26:20 -0600

    Adding webpacking with the main.js file

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,18 @@
-var canvas = document.getElementById("photo"),
-    context = canvas.getContext('2d');
+const onload = () => {
+  const canvas = document.getElementById("photo"),
+    context = canvas.getContext("2d");
+  context.beginPath();
+  context.arc(75, 75, 75, 0, Math.PI * 2, true);
+  context.clip();
+  const img = new Image();
+  img.onload = () => {
+    context.drawImage(img, -60, 0);
+    context.lineWidth = 15;
+    context.strokeStyle = "#000";
+    context.stroke();
+  };
+  img.src = "../images/gxldcptrickE.jpg";
+};
 
-context.beginPath();
-context.arc(
-    75,
-    75,
-    75,
-    0,
-    Math.PI * 2,
-    true);
-context.clip();
-var img = new Image();
-img.onload = function() {
-        context.drawImage(img, -60, 0);
-        context.lineWidth = 15;
-        context.strokeStyle = '#000';
-        context.stroke();
-    } ;    
-img.src = '../images/gxldcptrickE.jpg';
+window.addEventListener("load", onload);
+document.addEventListener("load", onload);
